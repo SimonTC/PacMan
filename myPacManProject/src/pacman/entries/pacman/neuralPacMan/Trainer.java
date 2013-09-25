f2289972450cb3bc2108a4d205cb2d30cf1bb4e8
Simon T. Clement <STClementGit@gmail.com>
2013-09-25 22:35:58 +0200

    Started working on the trainer. WOtk on roundDown method

--- myPacManProject/src/pacman/entries/pacman/neuralPacMan/Trainer.java
+++ myPacManProject/src/pacman/entries/pacman/neuralPacMan/Trainer.java
@@ -1,6 +1,33 @@
 package pacman.entries.pacman.neuralPacMan;
 
+import dataRecording.DataSaverLoader;
+import dataRecording.DataTuple;
+
 public class Trainer {
 
+	public void train(){
+		//Do something
+	}
 	
+	private void loadData(){
+		DataSaverLoader dsl = new DataSaverLoader();
+		DataTuple[] data = dsl.LoadPacManData();
+		
+		for (DataTuple dt : data){
+			
+		}
+	}
+	
+	private double roundDown (double d){
+		if (isBetween(d, 0.0d, 0.01d)) {
+			return 0.0d;
+		}
+	}
+	
+	private boolean isBetween(double d, double lBound, double uBound){
+		if (d >= lBound && d < uBound){
+			return true;
+		}
+		return false;
+	}
 }
